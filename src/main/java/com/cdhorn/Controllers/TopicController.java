4861e9957de29d19a4f4f1ff3c101155ff5da094
Christina Cornell <cdhorn515@gmail.com>
2017-07-31 10:13:54 -0400

    data hardcoded and added, rendering to screen through th:

--- src/main/java/com/cdhorn/Controllers/TopicController.java
+++ src/main/java/com/cdhorn/Controllers/TopicController.java
@@ -1,13 +1,27 @@
 package com.cdhorn.Controllers;
 
+import com.cdhorn.models.Topic;
 import org.springframework.stereotype.Controller;
+//want spring frameworks Model import
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class TopicController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        Topic first = new Topic(1, "Java Spring", "Not sure how spring works");
+        Topic second = new Topic(2, "Backend Fundamentals", "Get me out of Node!");
+        Topic third = new Topic (3, "Java", "Java Bean and Jelly Bean");
+        ArrayList<Topic> topics = new ArrayList<>();
+        topics.add(first);
+        topics.add(second);
+        topics.add(third);
+        model.addAttribute("topics", topics);
 
         return "index";
     }
